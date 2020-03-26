ba8e38df61b8cffbece3e60880535fcb0bda5d0c
armand f <armandfarizzi@gmail.com>
2020-03-26 21:02:07 +0700

    dev2

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Flutter is Fun'),
-//      centerTitle: true,
-    ),
-    body: Center(
-      child: Text("Hello from flutter"),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter is Fun'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[500],
+      ),
+      body: Center(
+        child: Image.asset('assets/city.jfif')
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        backgroundColor: Colors.grey[500],
+      ),
+    );
+  }
+}
 
